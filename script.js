3825a1523f2df30e5c3ed7da8955a8e459ad98ee
piyushjain5350 <126599802+piyushjain5350@users.noreply.github.com>
2023-09-08 16:42:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,47 @@
-const itemName=document.getElementById('item-name-input');
-const itemPrice=document.getElementById('item-price-input');
-const addItemBtn=document.getElementById('add');
-const tableAddItem=document.getElementById('add-item');
-const total=document.getElementById('total');
-
-var totalVal=0;
-
-
-addItemBtn.addEventListener('click',()=>{
-    if(itemName.value!=''|| itemPrice.value!='' ){
-        const tr=document.createElement('tr');
-        console.log(typeof Number(itemPrice.value))
-        tr.innerHTML=`
-         <td>${itemName.value}</td>
-         <td>${itemPrice.value}</td>
-        `
-        tableAddItem.appendChild(tr);
-        totalVal=totalVal+Number(itemPrice.value);
-        total.innerText=`Grand total => ${totalVal}`;
-
-        itemName.value='';
-        itemPrice.value='';
-        console.log(totalVal);
-    }
-})
-
-
+//your code here
+var itemName = document.getElementById("item-name-input");
+var itemQuantity = document.getElementById("item-qty-input");
+var itemPrice = document.getElementById("item-price-input");
+ 
+var tbody = document.querySelector("tbody");
+ 
+var grandTotal = document.getElementById("total");
+var grand_total = 0;
+ 
+var addItem = document.querySelector("#add");
+ 
+addItem.addEventListener("click", (event) => {
+  event.preventDefault();
+ 
+  let newRow = document.createElement("tr");
+ 
+  let name = document.createElement("td");
+  name.innerText = itemName.value;
+  name.classList.add("item");
+  newRow.append(name);
+ 
+  let price = document.createElement("td");
+  price.innerText = itemPrice.value;
+  newRow.append(price);
+  price.classList.add("price");
+ 
+  let quantity = document.createElement("td");
+  quantity.innerText = itemQuantity.value;
+  newRow.append(quantity);
+ 
+  if (itemName.value && itemQuantity.value && itemPrice.value) {
+    let total = document.createElement("td");
+    total.innerText = itemPrice.value * itemQuantity.value;
+    newRow.append(total);
+ 
+    tbody.append(newRow);
+ 
+    grand_total += Number(total.innerText);
+    grandTotal.innerHTML = "Grand Total: ₹ " + grand_total;
+  }
+ 
+  itemName.value = "";
+  itemQuantity.value = "";
+  itemPrice.value = "";
+});
+ 
