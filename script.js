94c0fa93aa8ea3213eabfd28f60e5bd99ff29980
piyushjain5350 <126599802+piyushjain5350@users.noreply.github.com>
2023-09-08 16:28:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your code here
+const itemName=document.getElementById('item-name-input');
+const itemPrice=document.getElementById('item-price-input');
+const addItemBtn=document.getElementById('add');
+const tableAddItem=document.getElementById('add-item');
+const total=document.getElementById('total');
+
+var totalVal=0;
+
+
+addItemBtn.addEventListener('click',()=>{
+    if(itemName.value!=''|| itemPrice.value!='' ){
+        const tr=document.createElement('tr');
+        console.log(typeof Number(itemPrice.value))
+        tr.innerHTML=`
+         <td>${itemName.value}</td>
+         <td>${itemPrice.value}</td>
+        `
+        tableAddItem.appendChild(tr);
+        totalVal=totalVal+Number(itemPrice.value);
+        total.innerText=`Grand total => ${totalVal}`;
+
+        itemName.value='';
+        itemPrice.value='';
+        console.log(totalVal);
+    }
+})
+
+
